aa86d1ac06258ed2f43ed3df218afa0e8c0b7a2e
whylineee <msavin901001@gmail.com>
2026-01-30 20:39:13 +0200

    Initial Commit

---
+++ HireNowOnSwift/HireNowOnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HireNowOnSwiftApp.swift
+//  HireNowOnSwift
+//
+//  Created by Marko on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HireNowOnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
