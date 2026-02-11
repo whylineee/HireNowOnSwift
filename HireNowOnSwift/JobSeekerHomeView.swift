e09c7e38f9deeea357689a42dbbdff724258a522
whylineee <msavin901001@gmail.com>
2026-02-11 16:01:43 +0200

    fake n2

--- HireNowOnSwift/JobSeekerHomeView.swift
+++ HireNowOnSwift/JobSeekerHomeView.swift
@@ -19,7 +19,7 @@ struct JobSeekerHomeView: View {
                     }
                 }
             }
-            .navigationTitle("Jobs")
+            .navigationTitle("JobS")
         }
     }
 }
